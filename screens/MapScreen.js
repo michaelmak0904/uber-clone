b3fb7bacb33e310da0d5556abb7203c193e9273d
michaelmak0904 <michaelmak0904@gmail.com>
2022-07-31 13:36:41 +0800

    implementing travel time and distance

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import Map from "../components/Map"
 import NavigateCard from "../components/NavigateCard"
 import RideOptionsCard from "../components/RideOptionsCard"
 import { createStackNavigator } from "@react-navigation/stack"
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 export default function MapScreen() {
 
     const Stack = createStackNavigator()
+    const navigation = useNavigation()
 
     return (
         <View>
+
+            <TouchableOpacity style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full`}
+                onPress={() => navigation.navigate("HomeScreen")}>
+                <Icon name="menu" />
+            </TouchableOpacity>
             <View style={tw`h-1/2`}>
                 <Map />
             </View>
